0092a57afc34e0cd62daa5aff928779ac0d60a29
Brandon Ooi <brandono@gmail.com>
2022-03-25 16:50:39 -0700

    additianal clarification on the behavior

--- javascript/TimeMap.js
+++ javascript/TimeMap.js
@@ -17,6 +17,7 @@ class TimeMap {
 
 // timeMap.set('foo', 'bar', 1);
 // timeMap.get('foo', 1); //returns 'bar'
+// timeMap.get('foo', 3); //returns 'bar'
 // timeMap.set('foo', 'baz', 3);
 // timeMap.get('foo', 1); // returns 'bar'
 // timeMap.get('foo', 2); // returns 'bar'
